fbc96e2612e04bbfe2f60461f21e6aa0bdc2fa84
choong <choong@co2050-23.ece.iastate.edu>
2025-04-16 18:27:22 -0500

    adding proj2

--- Proj 2/newcpu/cpre381-toolflow/cpre381-toolflow/proj/src/TopLevel/control_package.vhd
+++
@@ -1,6 +0,0 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
-package control_package is
-    type mux_bus_control is array (15 downto 0) of std_logic_vector(31 downto 0);
-end package control_package;
